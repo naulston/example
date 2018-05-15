b33cd62d4cd2713cedefb6a6528d122d4dc1be06
naulston <naulston@umich.edu>
2018-05-15 00:31:01 -0700

    updated site
    total transformation

---
+++ js/functions.js
@@ -0,0 +1,65 @@
+// JavaScript Functions ( Statti Template )
+
+$(document).ready(function () {
+
+  /* --- Active Filter Menu --- */
+  $(".switch-section a i, .filter a").click(function (e) {
+    $(".switch-section a i, .filter a").removeClass("active");
+    $(this).addClass("active");
+    e.preventDefault();
+  });
+  
+  /* --- Masonry --- */
+  var $container = $(".masonry");
+  $container.imagesLoaded(function () {
+    $container.isotope({
+      itemSelector: ".item",
+    });
+  });
+  $("#folio-filters a, #blog-filters a").click(function () {
+    var selector = $(this).attr("data-filter");
+    $container.isotope({
+      filter: selector
+    });
+    return false;
+  });
+
+  /* --- Item Description --- */
+  $(".item").click(function () {
+    $(this).toggleClass("open");
+  })
+  
+  /* --- Fancybox --- */
+  $(".view-fancybox").fancybox({
+    openEffect: 'elastic',
+    closeEffect: 'elastic',
+    next: '<i class="icon-smile"></i>',
+    prev: '<a title="Previous" class="fancybox-nav fancybox-prev" href="javascript:;"><span></span></a>'
+  });
+  
+  /* --- NiceScroll --- */
+  $(".section").niceScroll();
+  
+  /* --- Slider --- */
+  $('#slides').superslides({
+    slide_easing: 'easeInOutCubic',
+    slide_speed: 800,
+    play: 4000,
+    pagination: true,
+    hashchange: true,
+    scrollable: true
+  });
+
+});
+
+/* --- Flex Slider --- */
+$(window).load(function() {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    animationLoop: true,
+    itemWidth: 300,
+    itemMargin: 0,
+    prevText: "<i class='icon-angle-left'></i>",
+    nextText: "<i class='icon-angle-right'></i>",
+  });
+});
